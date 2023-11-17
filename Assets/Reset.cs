28893bc7e2679a8a298fff8b1193079f7dc608e2
Álvaro Hidalgo Riquelme <alvarohiri@gmail.com>
2023-11-17 10:26:10 +0100

    nivel

---
+++ Assets/Reset.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Reset : MonoBehaviour
+{
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+
+}
